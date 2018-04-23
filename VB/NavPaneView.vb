df4f94f17fd921368eef7cb22a4503d2f30e3aa4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/NavPaneView.vb
@@ -0,0 +1,42 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports DevExpress.Utils
+Imports DevExpress.Utils.Drawing
+Imports DevExpress.XtraNavBar
+Imports DevExpress.XtraNavBar.ViewInfo
+
+Namespace CustomNavigation
+	Public Class CustomNavPaneViewInfo
+		Inherits NavigationPaneViewInfo
+		Public Sub New(ByVal navBar As NavBarControl)
+			MyBase.New(navBar)
+		End Sub
+		Protected Overrides Function CreateNavPaneHeaderPainter() As NavigationPaneHeaderPainter
+			Return New CustomNavPaneHeaderPainter(Me)
+		End Function
+	End Class
+	Public Class CustomNavPaneHeaderPainter
+		Inherits NavigationPaneHeaderPainter
+		Public Sub New(ByVal npvi As NavigationPaneViewInfo)
+			MyBase.New(npvi)
+		End Sub
+
+		Public Overrides Overloads Function CalcObjectMinBounds(ByVal e As ObjectInfoArgs) As Rectangle
+			Return Rectangle.Empty
+		End Function
+	End Class
+	Public Class CustomNavPaneViewInfoRegistrator
+		Inherits NavigationPaneViewInfoRegistrator
+		Public Overrides ReadOnly Property ViewName() As String
+			Get
+				Return "CustomNavPaneView"
+			End Get
+		End Property
+		Public Overrides Function CreateViewInfo(ByVal navBar As NavBarControl) As NavBarViewInfo
+			Return New CustomNavPaneViewInfo(navBar)
+		End Function
+
+	End Class
+End Namespace
